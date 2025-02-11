e6b49ce1537f32c7c534b938aa422486566b8d60
MAINAK CHAUDHURI <mainakchaudhuri671@gmail.com>
2025-02-11 07:49:44 +0530

    Update auto-restart.py

--- auto-restart.py
+++ auto-restart.py
@@ -2,7 +2,7 @@ import streamlit as st
 import datetime
 
 # Function to check last restart time
-@st.cache_resource(ttl=86400)  # Auto-refresh after 24 hours
+@st.cache_resource(ttl=8)  # Auto-refresh after 24 hours
 def last_restart_time():
     return datetime.datetime.now()
 
